8312c8d2a3ddb8f2272d4184a017af4118b510ab
Bartłomiej Mumot <mumot1999@gmail.com>
2022-01-21 19:23:32 +0100

    cache and select afted union

--- src/main/scala/Places.scala
+++ src/main/scala/Places.scala
@@ -10,23 +10,27 @@ case class Places(citiesListingDS: Cities[DataFrame]) {
       .distinct()
       .withColumn("city", lit("Berlin"))
       .select("neighbourhood", "neighbourhood_group", "city")
+      .cache()
 
     val parisNeighbourhoodsDS = citiesListingDS.paris
       .select("neighbourhood", "neighbourhood_group")
       .distinct()
       .withColumn("city", lit("Paris"))
       .select("neighbourhood", "neighbourhood_group", "city")
+      .cache()
 
     val madridNeighbourhoodsDS = citiesListingDS.madrid
       .select("neighbourhood", "neighbourhood_group")
       .distinct()
       .withColumn("city", lit("Madrid"))
       .select("neighbourhood", "neighbourhood_group", "city")
+      .cache()
 
     parisNeighbourhoodsDS
       .union(madridNeighbourhoodsDS)
       .union(berlinNeighbourhoodsDS)
       .withColumn("id", monotonically_increasing_id + 1)
+      .select("id","neighbourhood", "neighbourhood_group", "city")
       .write
       .insertInto("places")
   }
