edfd9730de977cc31047c17da8660ec64f22fdb4
Bartłomiej Mumot <mumot1999@gmail.com>
2022-01-21 10:41:47 +0100

    drop columns from MadridCalendar

--- src/main/scala/Facts.scala
+++ src/main/scala/Facts.scala
@@ -13,13 +13,17 @@ object Facts {
       .config("spark.master", "local")
       .getOrCreate()
 
-    def readCsvFile(fileName: String) = {
+    def getDF(fileName: String) = {
       sqlContext.read.format("org.apache.spark.csv")
         .option("header", true).option("inferSchema", true)
         .option("quote", "\"")
         .option("escape", "\"")
         .option("multiline", true)
         .csv(path + "/" + fileName)
+    }
+
+    def readCsvFile(fileName: String) = {
+      getDF(fileName)
         .cache()
     }
 
@@ -29,9 +33,12 @@ object Facts {
       .map(city => readCsvFile(s"${city}Listings.csv"))
       .reduce((a, b) => a.union(b))
 
-    val calendarsDS = cities
+    val calendarsDS = List("Berlin", "Paris")
       .map(city => readCsvFile(s"${city}Calendar.csv"))
       .reduce((a, b) => a.union(b))
-
+      .union(
+        getDF("MadridCalendar.csv")
+          .drop("adjusted_price", "minimum_nights", "maximum_nights")
+      )
   }
 }
