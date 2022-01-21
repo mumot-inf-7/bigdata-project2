9e1084e49f4e3a7651e5976c92d0f5d159f4c423
Bartłomiej Mumot <mumot1999@gmail.com>
2022-01-21 19:15:12 +0100

    union places to make unique id

--- src/main/scala/Places.scala
+++ src/main/scala/Places.scala
@@ -8,26 +8,26 @@ case class Places(citiesListingDS: Cities[DataFrame]) {
     val berlinNeighbourhoodsDS = citiesListingDS.berlin
       .select("neighbourhood", "neighbourhood_group")
       .distinct()
-      .withColumn("id", monotonically_increasing_id + 1)
       .withColumn("city", lit("Berlin"))
-      .select("id", "neighbourhood", "neighbourhood_group", "city")
+      .select("neighbourhood", "neighbourhood_group", "city")
 
     val parisNeighbourhoodsDS = citiesListingDS.paris
       .select("neighbourhood", "neighbourhood_group")
       .distinct()
-      .withColumn("id", monotonically_increasing_id + 1)
       .withColumn("city", lit("Paris"))
-      .select("id", "neighbourhood", "neighbourhood_group", "city")
+      .select("neighbourhood", "neighbourhood_group", "city")
 
     val madridNeighbourhoodsDS = citiesListingDS.madrid
       .select("neighbourhood", "neighbourhood_group")
       .distinct()
-      .withColumn("id", monotonically_increasing_id + 1)
       .withColumn("city", lit("Madrid"))
-      .select("id", "neighbourhood", "neighbourhood_group", "city")
+      .select("neighbourhood", "neighbourhood_group", "city")
 
-    berlinNeighbourhoodsDS.write.insertInto("places")
-    parisNeighbourhoodsDS.write.insertInto("places")
-    madridNeighbourhoodsDS.write.insertInto("places")
+    parisNeighbourhoodsDS
+      .union(madridNeighbourhoodsDS)
+      .union(berlinNeighbourhoodsDS)
+      .withColumn("id", monotonically_increasing_id + 1)
+      .write
+      .insertInto("places")
   }
 }
